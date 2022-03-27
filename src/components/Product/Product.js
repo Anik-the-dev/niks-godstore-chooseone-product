4add45c47aff52da102ffa4543b74dcb804895d5
Anik-the-dev <niksctg00@gmail.com>
2022-03-27 11:03:15 +0600

    cart btn add

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,12 +3,15 @@ import './Product.css'
 
 const Product = (props) => {
 
-    const {id,name,image,price} = props.product
+    const {name,image,price} = props.product
     return (
         <div className='singleProduct'>
             <img src={image} alt="Anik"/>
             <h4>Product: {name}</h4>
             <p>Product Price: ${price}</p>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
